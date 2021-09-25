6d6d76131522ba81403abd6774ed46345fbc5a87
Joseph Bacal <bacalj@gmail.com>
2021-09-24 23:48:00 -0400

    nearly there, commiting while not broken

--- components/Students.vue
+++ components/Students.vue
@@ -16,6 +16,10 @@ export default {
     }
   },
 
+  created(){
+    this.setInitialData()
+  },
+
   mounted(){
     this.readFromFirestore()
   },
@@ -31,14 +35,45 @@ export default {
     }
   },
 
+
+
+ /* ok lets do patches in moounted but initial load in asyncData or created() */
   methods: {
+    async setInitialData(){
+      this.$fire.firestore.collection("students").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          let studentObj = { id: doc.id, first: doc.data().first, last: doc.data().last, status: doc.data().status }
+          this.allStudents.push(studentObj)
+        });
+      });
+    },
+
     async readFromFirestore() {
       const studentsRef = this.$fire.firestore.collection('students')
 
       studentsRef.onSnapshot((snap) =>{
         let changes = snap.docChanges()
         changes.forEach((change) => {
-          console.log(change.doc.data())
+
+          if (change.type === "modified") {
+
+            let idToChange = change.doc.id
+            let newStatus = change.doc.data().status
+
+            console.log(idToChange, " - should now be: ", newStatus)
+
+            // 1 find item in array where change.doc.id == item.id
+            const indexOfStudent = this.allStudents.findIndex((student) => {
+              return student.id == idToChange
+            })
+
+            console.log(indexOfStudent)
+
+            // 2 this.$set item.status to change.doc.data().status
+           // this.$set(this.allStudents[indexOfStudent], 'status', change.doc.data().status)
+          }
+
+
         })
       })
       // studentsRef.onSnapshot((snapshot) => {
@@ -50,15 +85,7 @@ export default {
 
       /* ok client is notified of change, just have to make store react, try it , but if not work, maybe a manual patch using the change object you get back from firestore like in this so-so SO example: https://stackoverflow.com/questions/62464830/updating-a-list-on-vuejs-by-using-the-real-time-change-listener-on-firestore */
 
-      // try {
-      //   const studentsCollection = await studentsRef.get()
-      //   console.log(studdentsCollection)
-      // }
 
-      // catch (e) {
-      //   alert(e)
-      //   return
-      // }
     }
   }
 }
